a621908ffaab6fd86cf21eaddacaa47c4ebfb8c9
Alara <0931871@hr.nl>
2018-12-14 15:57:12 +0100

    Go

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ if(process.env.ENV == 'Test')                                   //Als environmen
                                                                 //Als bookAPI niet bestaat dan creeert hij het nu gewoon zelf.
 else{
     db = mongoose.connect('mongodb://localhost/bookAPI');
-}
+};
 let Book = require('./models/bookModel');                       //Roep Model file op
 let app = express();                                            //Execute Express()
 let port = process.env.PORT || 3000;                            //Als er geen process.env.port (staat in gulpfile.js) aanwezig is doe port 3000. 
